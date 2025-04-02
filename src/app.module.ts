46259ced6ef72cece0d368ef15e8b93c8011b39e
Murilo Mazur <tads.murilomazur@gmail.com>
2025-04-02 11:37:37 -0300

    feat: adicionando envio de multiplos arquivos e modulo de email

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { AuthModule } from './auth/auth.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 
 @Module({
   imports: [
@@ -18,7 +20,20 @@ import { ConfigModule } from '@nestjs/config';
       }
     ]),
     forwardRef(() => PacienteModule),
-    forwardRef(() => AuthModule)
+    forwardRef(() => AuthModule),
+    MailerModule.forRoot({
+      transport: 'smtps://user@domain.com:pass@smtp.domain.com',
+      defaults: {
+        from: '"nest-modules" <modules@nestjs.com>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
     ],
   controllers: [AppController],
   providers: [AppService, {
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Controller, Headers, Param, Post, UploadedFile, UploadedFiles, UseGuards, UseInterceptors } from "@nestjs/common";
+import { BadRequestException, Body, Controller, FileTypeValidator, Headers, MaxFileSizeValidator, Param, ParseFilePipe, Post, UploadedFile, UploadedFiles, UseGuards, UseInterceptors } from "@nestjs/common";
 import { LoginDto } from "./dto/auth.login.dto";
 import { RegisterDto } from "./dto/auth.register.dto";
 import { ForgetDto } from "./dto/auth.forget.dto";
@@ -8,7 +8,7 @@ import { UsuarioService } from "src/usuario/usuario.service";
 import { AuthGuard } from "src/guards/auth.guard";
 import { DadosUsuario } from "src/decorators/usuario.decorator";
 import { ThrottlerGuard } from "@nestjs/throttler";
-import { FileInterceptor } from "@nestjs/platform-express";
+import { FileFieldsInterceptor, FileInterceptor, FilesInterceptor } from "@nestjs/platform-express";
 import { writeFile } from 'fs/promises'
 import { join } from "path";
 import { FileService } from "src/file/file.service";
@@ -57,7 +57,12 @@ export class AuthController {
     @UseInterceptors(FileInterceptor('file'))
     @UseGuards(AuthGuard)
     @Post('photo')
-    async photo(@DadosUsuario() usuario, @UploadedFile() arquivo: Express.Multer.File){
+    async photo(@DadosUsuario() usuario, @UploadedFile(new ParseFilePipe({
+        validators: [
+            new FileTypeValidator({fileType: 'image/*'}),
+            new MaxFileSizeValidator({maxSize: 1024 * 50})
+        ]
+    })) arquivo: Express.Multer.File){
 
         const path = join(__dirname,'..', '..', 'storage', 'photos', `photo-${usuario.id}.png`);
 
@@ -71,5 +76,27 @@ export class AuthController {
         return {sucess: true};
     }
 
+
+    @UseInterceptors(FilesInterceptor('files'))
+    @UseGuards(AuthGuard)
+    @Post('files')
+    async uploadFiles(@DadosUsuario() usuario, @UploadedFiles() arquivos: Express.Multer.File[]){
+        return arquivos;
+    }
+
+    @UseInterceptors(FileFieldsInterceptor([{
+        name: 'photo',
+        maxCount: 1
+    },
+    {
+        name: 'documents',
+        maxCount: 10
+    }
+]))
+    @UseGuards(AuthGuard)
+    @Post('files-fields')
+    async uploadFilesFields(@DadosUsuario() usuario, @UploadedFiles() arquivos: {photo: Express.Multer.File, documents: Express.Multer.File}){
+        return arquivos;
+    }
 
 }
